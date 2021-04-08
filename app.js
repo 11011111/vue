4653f6d4dd348caa6644c1d8432cf4883fe54960
Maxim <11011111@inbox.ru>
2021-04-09 00:26:21 +0300

    конец второй части

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-const App = {
-    data() {
-        return {
-            placeholderString: 'Введите новую заметку',
-            title: 'Список заметок',
-            inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
-        }
-    },
-    methods: {
-        addNewNote() {
-            if (this.inputValue !== ''){
+Vue.createApp({
+         data: () => ({
+              myHtml: '<h1>Vue 3 app</h1>',
+              title: 'Я есть Грут',
+              person: {
+                   firstName: 'Max',
+                   lastName: 'Khrustalev',
+                   age: 26
+              },
+              items: [1, 2]
+         }),
+         methods: {
+              addItem() {
+                   this.items.unshift(this.$refs.myInput.value)
+                   this.$refs.myInput.value = ''
+                   console.log(event.key)
+              }
+         },
+         computed: {
+              evenItems() {
 
-            this.notes.push(this.inputValue)
-            this.inputValue = ''
-            }
-        },
-        toUpperCase(item) {
-            return item.toUpperCase()
-        },
-        deleteNote(idx) {
-            this.notes.splice(idx, 1)
-        }
-    },
-    computed: {
-        doubleCountComputed() {
-            return this.notes.length * 2
-        },
-    },
-    watch: {
-        inputValue(value){
-            if (value.length > 10){
-               this.inputValue = ''
-            }
-            console.log('input value Change', value)
-        }
+                   return this.items.filter(i => i % 2 === 0)
+              }
+         }
     }
-}
-
-
-Vue.createApp(App).mount('#app')
-
+).mount('#app')
