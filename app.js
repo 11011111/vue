5171f5f158493f3f71b5ca5fd8c84c60909290d3
Maxim <11011111@inbox.ru>
2021-04-08 20:49:02 +0300

    конец первой части

--- app.js
+++ app.js
@@ -3,12 +3,36 @@ const App = {
         return {
             placeholderString: 'Введите новую заметку',
             title: 'Список заметок',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
         }
     },
     methods: {
-        inputChangeHandler(event) {
-            this.inputValue = event.target.value
+        addNewNote() {
+            if (this.inputValue !== ''){
+
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        deleteNote(idx) {
+            this.notes.splice(idx, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value){
+            if (value.length > 10){
+               this.inputValue = ''
+            }
+            console.log('input value Change', value)
         }
     }
 }
